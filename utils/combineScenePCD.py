0a5fb3b04f8e79f132f85ecc53084436f5b83140
ajaymopidevi <ajaymopidevi@gmail.com>
2025-11-03 14:12:27 -0500

    scores

--- utils/combineScenePCD.py
+++ utils/combineScenePCD.py
@@ -7,24 +7,11 @@ import math
 from scipy.spatial import cKDTree
 from sklearn.cluster import KMeans
 import numpy as np
-from extensions.chamfer_dist import ChamferDistanceL1, ChamferDistanceL2
+from UpPoinTr.extensions.chamfer_dist import ChamferDistanceL1, ChamferDistanceL2
 import torch
 
-# pcd_dir = "/home/ajay/ARPG/RMap/data/EncoderFeatureSampling"
-# pcd_dir = "/home/ajay/ARPG/RMap/RPoinTr/V100/SPoinTr_V100_AreaSampling_Pred_Normalize/"
-# pcd_dir = "/home/ajay/ARPG/IROS/Pred_UpPoinTr_PCNEncoderD4DecoderD6_Heads4Dim256_OnlyAttnGraph_54Split_BS16_Factors1_4_4_Var50InputCenter"
-#pcd_dir = "/media/giantdrive/kharlow/coloradar/RMap/PoinTr/ColoRadar/FPSRadar2/gt"
-pcd_dir = "/home/ajay/RMap/AdaPoinTr_FPSRadarLarge/"
-"""
-scene_names = [
-        "arpg_lab_run0_", "arpg_lab_run1_", "arpg_lab_run2_", "arpg_lab_run3_", "arpg_lab_run4_",
-        "aspen_run0_", "aspen_run1_", "aspen_run2_", "aspen_run3_", "aspen_run4_", "aspen_run5_", "aspen_run6_", "aspen_run7_", "aspen_run8_", "aspen_run9_", "aspen_run10_", "aspen_run11_",
-        "ec_hallways_run0_", "ec_hallways_run1_", "ec_hallways_run2_", "ec_hallways_run3_", "ec_hallways_run4_", 
-        "edgar_army_run0_", "edgar_army_run1_", "edgar_army_run2_", "edgar_army_run3_", "edgar_army_run4_", "edgar_army_run5_",
-        "edgar_classroom_run0_", "edgar_classroom_run1_", "edgar_classroom_run2_", "edgar_classroom_run3_", "edgar_classroom_run4_", "edgar_classroom_run5_",
-        "outdoors_run0_", "outdoors_run1_", "outdoors_run2_", "outdoors_run3_", "outdoors_run4_", "outdoors_run5_", "outdoors_run6_", "outdoors_run7_", "outdoors_run8_", "outdoors_run9_"
-        ]
-"""
+pcd_dir = "output/AdaPoinTr_FPSRadarLarge/"
+maps_dir = "gt_maps"
 scene_names = [
                 "arpg_lab_run0_", "arpg_lab_run1_",
                 "aspen_run0_", "aspen_run1_", "aspen_run2_", "aspen_run3_", "aspen_run4_", "aspen_run5_",
@@ -34,31 +21,11 @@ scene_names = [
                 "outdoors_run0_", "outdoors_run1_", "outdoors_run2_", "outdoors_run3_", "outdoors_run4_"
                ]
 
-# scene_names = [
-#                 "arpg_lab_run0_", "arpg_lab_run1_"
-#                ]
-
-# scene_names = ["G_canteen_circle_bright_1_0_","G_atrium_linear_bright_2_0_", "G_corridor_linear_bright_11_0_", "G_corridor_linear_bright_13_0_", "G_corridor_linear_bright_18_0_", "G_hallway_linear_bright_1_0_", "G_hallway_linear_bright_3_0_", "G_stairwell_linear_bright_1_0_", "G_vicon_eight_bright_1_0_", "G_vicon_eight_bright_5_0_"]
 voxel_size = 0.15
 files = sorted(os.listdir(pcd_dir))
-print(len(files))
-lidar_dir = "gt_maps"
-pts_count =0
-# def postprocess(pcd):
-#     kdtree = o3d.geometry.KDTreeFlann(pcd)
-#     seed_point = [0,0,0]
-#     [k, idx, _] = kdtree.search_knn_vector_xd(seed_point, len(pcd.points))
-#     patch_points = np.asarray(points)[idx[::3], :]
-#     patch_pcd = o3d.geometry.PointCloud()
-#     patch_pcd.points = o3d.utility.Vector3dVector(patch_points)
-#     return patch_pcd
 
 def postprocess(pcd):
-    # print("Radar Input", np.array(pcd.points).shape)
     pcd, ind = pcd.remove_statistical_outlier(nb_neighbors=64,std_ratio=2)
-    # filtered_pcd = o3d.geometry.PointCloud()
-    # filtered_pcd.points = o3d.utility.Vector3dVector(pcd.points[ind,:])
-    # print("Radar filtering", np.array(pcd.points).shape)    
     return pcd
 
 def get_fscore(pred, gt):
@@ -72,23 +39,14 @@ def get_fscore(pred, gt):
     return result
 
 
-input_L1 = []
-input_L2 = []
-input_fscore = []
-input_avg_d = []
-
-
-L1 = []
-L2 = []
-fscore = []
-avg_d = []
-
-filtered_L1 = []
-filtered_L2 = []
-filtered_fscore = []
-filtered_avg_d = []
 
 if __name__ == "__main__":
+    
+    L1 = []
+    L2 = []
+    fscore = []
+    avg_d = []
+
     CD_L2 = ChamferDistanceL2(ignore_zeros=True)
     CD_L1 = ChamferDistanceL1(ignore_zeros=True)
     for scene in scene_names:
@@ -100,39 +58,22 @@ if __name__ == "__main__":
         if(len(pcd_files)==0):
             continue
         
-        for i,f in enumerate(pcd_files):
-            
+        for i,f in enumerate(pcd_files):    
             pcd = o3d.io.read_point_cloud(f)
             centroid = pcd.points[0]
-            # pcd = pcd.translate(-centroid)
-            # print(pcd.get_center(), centroid)
             pcd = pcd.voxel_down_sample(voxel_size=voxel_size)
-            # pcd = pcd.translate(centroid)
             points.extend(pcd.points)
-            # pcd_points = np.load(f)
-            # print(pcd_points.shape)
-            # points.extend(pcd_points)
-
+        
             if i %100 == 1:
                 scene_pcd.points = o3d.utility.Vector3dVector(points)
                 scene_pcd = scene_pcd.voxel_down_sample(voxel_size=voxel_size)
                 points = scene_pcd.points
         
         scene_pcd.points = o3d.utility.Vector3dVector(np.unique(points, axis=0))
-        print(len(scene_pcd.points))
-        # scene_pcd, ind = scene_pcd.remove_statistical_outlier(nb_neighbors=50, std_ratio=2.0)
         
-        print(len(scene_pcd.points))
         scene_pcd = scene_pcd.voxel_down_sample(voxel_size=voxel_size)
-        print("Final Combined Scene points", len(scene_pcd.points))
-        #o3d.io.write_point_cloud(os.path.join(pcd_dir, scene+"recon.pcd"), scene_pcd, True, True)
-        # if scene == "aspen_run1_":
-        #     scene = "aspen_run1"
-        pcd = o3d.io.read_point_cloud(os.path.join(lidar_dir, scene+"lidar_filtered.ply"))
-        print("Lidar Scene Points", len(pcd.points))
-        pts_count = pts_count + len(pcd.points) - len(scene_pcd.points)
+        pcd = o3d.io.read_point_cloud(os.path.join(maps_dir, scene+"lidar_filtered.ply"))
         d = pcd.compute_point_cloud_distance(scene_pcd)
-        print(np.sum(d))
         input = torch.from_numpy(np.array(scene_pcd.points)).float().unsqueeze(0).cuda()
         gt = torch.from_numpy(np.array(pcd.points)).float().unsqueeze(0).cuda()
         L1.append(CD_L1(input, gt).detach().cpu().numpy())
@@ -140,75 +81,14 @@ if __name__ == "__main__":
         res = get_fscore(scene_pcd, pcd)
         fscore.append(res)
 
-
-
         print(f"Recon Scene: {scene} CD_L1: {L1[-1]} CD_L2: {L2[-1]}, F-Score: {res}")
         q = np.array([50,75,90,95])
 
         d = pcd.compute_point_cloud_distance(scene_pcd)
         avg_d.extend(d)   
         print(f"  Scene: {scene} Noise: {np.percentile(d, q=q)}")
-        """
-
-
-        #### Input Scene
-        radar_pcd = o3d.io.read_point_cloud(os.path.join(lidar_dir, scene+"radar.ply"))
-        input = torch.from_numpy(np.array(radar_pcd.points)).float().unsqueeze(0).cuda()
-        
-        input_L1.append(CD_L1(input, gt).detach().cpu().numpy())
-        input_L2.append(CD_L2(input, gt).detach().cpu().numpy())
-        res = get_fscore(radar_pcd, pcd)
-        input_fscore.append(res)
-
-        print(f"Radar Scene: {scene} CD_L1: {input_L1[-1]} CD_L2: {input_L2[-1]}, F-Score: {res}")
-        q = np.array([50,75,90,95])
-
-        d = pcd.compute_point_cloud_distance(radar_pcd)
-        input_avg_d.extend(d)
-        print(f"Radar  Scene: {scene} Noise: {np.percentile(d, q=q)}")
-        
-
-        # radar_pcd = o3d.io.read_point_cloud(os.path.join(lidar_dir, scene+"radar.ply"))
-        # input = torch.from_numpy(np.array(radar_pcd.points)).float().unsqueeze(0).cuda()
-        # print(f"Radar Scene: {scene} CD_L1: {CD_L1(input, gt)} CD_L2: {CD_L2(input, gt)}")
-        filtered_pcd = postprocess(scene_pcd)
-        print("Filtered Combined Scene points", len(filtered_pcd.points))
-        # o3d.io.write_point_cloud(os.path.join(pcd_dir, scene+"recon_filtered.pcd"), filtered_pcd, True, True)
-        # # if scene == "aspen_run1_":
-        # #     scene = "aspen_run1"
-        # d = pcd.compute_point_cloud_distance(filtered_pcd)
-        # print(np.sum(d))
-        input = torch.from_numpy(np.array(filtered_pcd.points)).float().unsqueeze(0).cuda()
-        gt = torch.from_numpy(np.array(pcd.points)).float().unsqueeze(0).cuda()
-        filtered_L1.append(CD_L1(input, gt).detach().cpu().numpy())
-        filtered_L2.append(CD_L2(input, gt).detach().cpu().numpy())
-        res = get_fscore(filtered_pcd, pcd)
-        filtered_fscore.append(res)
-
-
-
-        print(f"Filtered Scene: {scene} CD_L1: {L1[-1]} CD_L2: {L2[-1]}, F-Score: {res}")
-        d = pcd.compute_point_cloud_distance(filtered_pcd)
-        filtered_avg_d.extend(d)
-        print(f"Filtered  Scene: {scene} Noise: {np.percentile(d, q=q)}")
-        """
-        # print(f"Scene: {scene} CD_L1: {CD_L1(input, gt)} CD_L2: {CD_L2(input, gt)}")
-
-"""
-print("Overall Input")
-print(np.mean(input_L1), np.mean(input_L2), np.mean(input_fscore))
-q = np.array([50,75,90,95])
-print("Input Avg DD: ",np.percentile(input_avg_d,q=q))
-"""
 
 print("Overall Reconstruction")
-print(np.mean(L1), np.mean(L2), np.mean(fscore))
+print(f"CD_L1: {np.mean(L1)}, CD_L2:{np.mean(L2)}, F-SCORE: {np.mean(fscore)}")
 q = np.array([50,75,90,95])
 print("Avg DD: ",np.percentile(avg_d,q=q))
-print("Total missed points", pts_count)
-"""
-print("Overall Filtered")
-print(np.mean(filtered_L1), np.mean(filtered_L2), np.mean(filtered_fscore))
-q = np.array([50,75,90,95])
-print("Filtered Avg DD: ",np.percentile(filtered_avg_d,q=q))
-"""
--- utils/generateScores.py
+++ utils/generateScores.py
@@ -1,5 +1,3 @@
-# Combine prediction from model to generate the entire map and assess the performance
-
 import os
 import sys  
 import time
@@ -9,22 +7,12 @@ import math
 from scipy.spatial import cKDTree
 from sklearn.cluster import KMeans
 import numpy as np
-from extensions.chamfer_dist import ChamferDistanceL1, ChamferDistanceL2, ChamferDeviationL2
+from UpPoinTr.extensions.chamfer_dist import ChamferDistanceL1, ChamferDistanceL2, ChamferDeviationL2
 import torch
 
-gt_dir = "UpPoinTr/data/ColoRadar/gt"
-pred_dir = "output/UpPoinTr_FPSRadarLarge/"
-lidar_dir = "gt_maps"
-"""
-scene_names = [
-        "arpg_lab_run0_", "arpg_lab_run1_", "arpg_lab_run2_", "arpg_lab_run3_", "arpg_lab_run4_",
-        "aspen_run0_", "aspen_run1_", "aspen_run2_", "aspen_run3_", "aspen_run4_", "aspen_run5_", "aspen_run6_", "aspen_run7_", "aspen_run8_", "aspen_run9_", "aspen_run10_", "aspen_run11_",
-        "ec_hallways_run0_", "ec_hallways_run1_", "ec_hallways_run2_", "ec_hallways_run3_", "ec_hallways_run4_", 
-        "edgar_army_run0_", "edgar_army_run1_", "edgar_army_run2_", "edgar_army_run3_", "edgar_army_run4_", "edgar_army_run5_",
-        "edgar_classroom_run0_", "edgar_classroom_run1_", "edgar_classroom_run2_", "edgar_classroom_run3_", "edgar_classroom_run4_", "edgar_classroom_run5_",
-        "outdoors_run0_", "outdoors_run1_", "outdoors_run2_", "outdoors_run3_", "outdoors_run4_", "outdoors_run5_", "outdoors_run6_", "outdoors_run7_", "outdoors_run8_", "outdoors_run9_"
-        ]
-"""
+gt_dir = "UpPoinTr/data/CooloRadar/gt"
+pcd_dir = "output/AdaPoinTr_FPSRadarLarge/"
+maps_dir = "gt_maps"
 scene_names = [
                 "arpg_lab_run0_", "arpg_lab_run1_",
                 "aspen_run0_", "aspen_run1_", "aspen_run2_", "aspen_run3_", "aspen_run4_", "aspen_run5_",
@@ -35,12 +23,8 @@ scene_names = [
                ]
 
 voxel_size = 0.15
-files = sorted(os.listdir(pred_dir))
-print(len(files))
-pts_count =0
-
+files = sorted(os.listdir(pcd_dir))
 def postprocess(pcd):
-    # print("Radar Input", np.array(pcd.points).shape)
     pcd, ind = pcd.remove_statistical_outlier(nb_neighbors=64,std_ratio=2)
     return pcd
 
@@ -48,30 +32,23 @@ def get_fscore(pred, gt):
     th = 0.15
     dist1 = pred.compute_point_cloud_distance(gt)
     dist2 = gt.compute_point_cloud_distance(pred)
-
     recall = float(sum(d < th for d in dist2)) / float(len(dist2))
     precision = float(sum(d < th for d in dist1)) / float(len(dist1))
     result = 2 * recall * precision / (recall + precision) if recall + precision else 0.
     return result
 
 
-input_L1 = []
-input_L2 = []
-input_fscore = []
-input_avg_d = []
+if __name__ == "__main__":    
+    pred_L1 = []
+    pred_L2 = []
+    pred_fscore = []
+    pred_avg_dev = []
 
+    input_L1 = []
+    input_L2 = []
+    input_fscore = []
+    input_avg_dev = []
 
-L1 = []
-L2 = []
-fscore = []
-avg_d = []
-
-down_L1 = []
-down_L2 = []
-down_fscore = []
-down_avg_d = []
-
-if __name__ == "__main__":
     CD_L2 = ChamferDistanceL2(ignore_zeros=True)
     CD_L1 = ChamferDistanceL1(ignore_zeros=True)
     CD_L2Dev = ChamferDeviationL2(ignore_zeros=True)
@@ -80,23 +57,21 @@ if __name__ == "__main__":
     scene_L2 = []
     scene_fscore = []
     for scene in scene_names:
-        
         points = []
         scene_pcd = o3d.geometry.PointCloud()
         pcd_files = [f for f in files if f.endswith("_fine.pcd") and scene in f]
         gt_files = [os.path.join(gt_dir, f.split("_fine")[0]+".pcd") for f in pcd_files]
-        pcd_files = [os.path.join(pred_dir, f) for f in pcd_files]
-        
-        print(scene, len(pcd_files), len(gt_files))
+        pcd_files = [os.path.join(pcd_dir, f) for f in pcd_files]
         if(len(pcd_files)==0):
             continue
         
-        for i,f in enumerate(pcd_files):
-            
+        for i,f in enumerate(pcd_files):    
+            # Calculate Patch Scores
             pcd = o3d.io.read_point_cloud(pcd_files[i])
             gt_pcd = o3d.io.read_point_cloud(gt_files[i])
             pcd_points = np.array(pcd.points)
             gt_points = np.array(gt_pcd.points)
+            # Normalize points for Patch-level comparision
             centroid = pcd_points[0]
             pcd_points = pcd_points - centroid
             pcd_points = pcd_points/25.0
@@ -104,55 +79,60 @@ if __name__ == "__main__":
             gt_points = gt_points - centroid
             gt_points = gt_points/25.0
             gt_points = np.clip(gt_points, -1, 1)
-            #print(len(pcd.points), len(gt_pcd.points))
+            
             input = torch.from_numpy(np.array(pcd_points)).float().unsqueeze(0).cuda()
             gt = torch.from_numpy(np.array(gt_points)).float().unsqueeze(0).cuda()
             scene_L1.append(CD_L1(input, gt).detach().cpu().numpy())
             scene_L2.append(CD_L2(input, gt).detach().cpu().numpy())
             res = get_fscore(pcd, gt_pcd)
             scene_fscore.append(res)
-         
-        #print("Patch scores for ", scene, np.mean(scene_L1), np.mean(scene_L2), np.mean(scene_fscore))
-        down_scene_pcd = o3d.io.read_point_cloud(os.path.join(pred_dir, scene+"recon.pcd"))
-        #print(len(scene_pcd.points))
+
+            
+        down_scene_pcd = o3d.io.read_point_cloud(os.path.join(pcd_dir, scene+"recon.pcd"))
         down_scene_pcd = down_scene_pcd.voxel_down_sample(voxel_size=voxel_size)
-        print("Final Combined Scene points", len(down_scene_pcd.points))
-        #o3d.io.write_point_cloud(os.path.join(pred_dir, scene+"recon.pcd"), scene_pcd, True, True)
-        # if scene == "aspen_run1_":
-        #     scene = "aspen_run1"
-        pcd = o3d.io.read_point_cloud(os.path.join(lidar_dir, scene+"lidar_filtered.ply"))
-        #print("Lidar Scene Points", len(pcd.points))
-        pts_count = pts_count + len(pcd.points) - len(down_scene_pcd.points)
+        pcd = o3d.io.read_point_cloud(os.path.join(maps_dir, scene+"lidar_filtered.ply"))
         d = pcd.compute_point_cloud_distance(down_scene_pcd)
-        #print(np.sum(d))
+
+        # Calculate scores for reconstructed pcd
         input = torch.from_numpy(np.array(down_scene_pcd.points)).float().unsqueeze(0).cuda()
         gt = torch.from_numpy(np.array(pcd.points)).float().unsqueeze(0).cuda()
         ret = CD_L2Dev(input, gt).detach().cpu().numpy()
-        down_L1.append(CD_L1(input, gt).detach().cpu().numpy())
-        down_L2.append(CD_L2(input, gt).detach().cpu().numpy())
+        pred_L1.append(CD_L1(input, gt).detach().cpu().numpy())
+        pred_L2.append(CD_L2(input, gt).detach().cpu().numpy())
         res = get_fscore(down_scene_pcd, pcd)
-        down_fscore.append(res)
-
-
-
-        print(f"Recon Scene: {scene} CD_L1: {down_L1[-1]} CD_L2: {down_L2[-1]}, F-Score: {res}, Dist: {ret}")
+        pred_fscore.append(res)
+        print(f"Recon Scene: {scene} CD_L1: {pred_L1[-1]} CD_L2: {pred_L2[-1]}, F-Score: {res}, Dist: {ret}")
         q = np.array([50,75,90,95])
-
         d = pcd.compute_point_cloud_distance(down_scene_pcd)
-        down_avg_d.extend(d)   
+        pred_avg_dev.extend(d)   
         print(f"  Scene: {scene} Noise: {np.percentile(d, q=q)}")
         print("Patch scores for ", np.mean(scene_L1), np.mean(scene_L2), np.mean(scene_fscore))
 
+        #### Compare with Input Scene
+        radar_pcd = o3d.io.read_point_cloud(os.path.join(maps_dir, scene+"radar.ply"))
+        input = torch.from_numpy(np.array(radar_pcd.points)).float().unsqueeze(0).cuda()
+        
+        input_L1.append(CD_L1(input, gt).detach().cpu().numpy())
+        input_L2.append(CD_L2(input, gt).detach().cpu().numpy())
+        ret = CD_L2Dev(input, gt).detach().cpu().numpy()
+        res = get_fscore(radar_pcd, pcd)
+        input_fscore.append(res)
+
+        print(f"Radar Scene: {scene} CD_L1: {input_L1[-1]} CD_L2: {input_L2[-1]}, F-Score: {res}, Dist: {ret}")
+        q = np.array([50,75,90,95])
+        d = pcd.compute_point_cloud_distance(radar_pcd)
+        input_avg_dev.extend(d)
+        print(f"Radar  Scene: {scene} Noise: {np.percentile(d, q=q)}")
 
 
-
-
-
+print("Overall Input")
+print(f"CD_L1: {np.mean(input_L1)}, CD_L2:{np.mean(input_L2)}, F-SCORE: {np.mean(input_fscore)}")
+q = np.array([50,75,90,95])
+print("Input Avg DD: ",np.percentile(input_avg_dev,q=q))
 
 print("Overall Reconstruction")
-print(np.mean(down_L1), np.mean(down_L2), np.mean(down_fscore))
+print(f"CD_L1: {np.mean(pred_L1)}, CD_L2:{np.mean(pred_L2)}, F-SCORE: {np.mean(pred_fscore)}")
 q = np.array([50,75,90,95])
-print("Avg DD: ",np.percentile(down_avg_d,q=q))
-print("Total missed points", pts_count)
-print("Patch scores for ", np.mean(scene_L1), np.mean(scene_L2), np.mean(scene_fscore))
+print("Avg DD: ",np.percentile(pred_avg_dev,q=q))
+print(f"Patch scores for  CD_L1: {np.mean(scene_L1)}, CD_L2: {np.mean(scene_L2)}, F-SCORE: {np.mean(scene_fscore)}")
 
